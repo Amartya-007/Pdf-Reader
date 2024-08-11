8dd77448320bda0d40bee24158c4282ac1c94224
Amartya-007 <amartya1820@gmail.com>
2024-08-12 00:49:08 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Generative AI",
+    version="0.1",
+    packages=find_packages(),
+    author="Amartya Vishwakarma",
+    author_email="Amartya1820@gmail.com",
+    packages = find_packages(),
+
+)
